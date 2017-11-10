7b6bd5406dc650a7370f175ca53d67d3aefdaf0d
Petr Sahula <spetex@icloud.com>
2017-11-10 14:08:38 +0100

    Update zshell config

---
+++ zshell/theme.zsh
@@ -0,0 +1,25 @@
+autoload -Uz vcs_info
+autoload -U colors && colors
+setopt promptsubst
+
+zstyle ':vcs_info:*' formats '%b'
+
+precmd () {
+  vcs_info
+
+  STATUS=$(command git status --porcelain 2> /dev/null | tail -n1)
+
+  if [[ -n $STATUS ]]; then
+    local git_branch='%F{red}$vcs_info_msg_0_'
+  else
+    local git_branch='%F{green}$vcs_info_msg_0_'
+  fi
+
+  RPROMPT="${git_branch}"
+
+  if [ "$ZSH_CLEAN_PATH_STYLE" = "1" ]; then
+    PROMPT="%F{yellow}%c%{$reset_color%} "
+  else
+    PROMPT="%F{yellow}%~%{$reset_color%} "
+  fi
+}
