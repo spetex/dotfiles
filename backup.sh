9cd1453cfef11eb320b5e53ed708a4458e45076b
Petr Sahula <sahulpet@gmail.com>
2020-09-08 19:21:01 +0200

    More backup stuff

--- backup.sh
+++ backup.sh
@@ -8,3 +8,15 @@ cp ~/.config/waybar/* waybar/ && echo "done"
 
 printf "Backup kitty config... "
 cp ~/.config/kitty/* kitty/ && echo "done"
+
+printf "Backup git config... "
+cp ~/.gitconfig git/gitconfig && echo "done"
+
+printf "Backup git ignore... "
+cp ~/.gitignore git/gitignore && echo "done"
+
+printf "Backup vimrc file... "
+cp ~/.vimrc vim/vimrc && echo "done"
+
+printf "Backup vim color schemes file... "
+cp -r ~/.vim/colors vim/ && echo "done"
--- vim/init.vim
+++
@@ -1,7 +0,0 @@
-# Put this into your ~/.config/nvim folder
-# https://neovim.io/doc/user/nvim.html#nvim-from-vim
-# https://vi.stackexchange.com/questions/12794/how-to-share-config-between-vim-and-neovim
-
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath=&runtimepath
-source ~/.vimrc
