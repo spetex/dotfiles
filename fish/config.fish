c3c3d82331ffcf407d7e1acc71bf644a2cf040f7
Petr Sahula <mail@petrsahula.cz>
2022-05-27 13:44:50 +0200

    Update dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,14 @@
-rvm default
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vim=nvim
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+load_nvm
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mars/.google-cloud-sdk/path.fish.inc' ]; . '/Users/mars/.google-cloud-sdk/path.fish.inc'; end
+
+source ~/.iterm2_shell_integration.fish
+
